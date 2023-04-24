8b8e2ad9234a590eaf505320a6a1c13a90ca3a0e
mikhail <m.buldakov@softwarecats.dev>
2023-04-24 22:06:54 +0500

    Updates

---
+++ assets/countries-65cfc65d.js
@@ -0,0 +1,1 @@
+const e=[{value:"Россия",label:"Россия",code:"RU"},{value:"Беларусь",label:"Беларусь",code:"BY"},{value:"Казахстан",label:"Казахстан",code:"KZ"},{value:"Армения",label:"Армения",code:"AM"},{value:"Грузия",label:"Грузия",code:"GR"}];export{e as c};
